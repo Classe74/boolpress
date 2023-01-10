b30e98862f29391cf634af40a9737af23ca6760a
GioGreco <grecogiorgio.97@gmail.com>
2023-01-10 12:43:47 +0100

    create templates post entity

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Create Post</h1>
+
+@endsection
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <ul>
+        @foreach ($posts as $post)
+            <li>{{$post->title}}</li>
+        @endforeach
+    </ul>
+
+@endsection
