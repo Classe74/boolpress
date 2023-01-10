4acf3c8aeef52869b31b0dd662398de5a6512c11
GioGreco <grecogiorgio.97@gmail.com>
2023-01-10 12:49:59 +0100

    add post route

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,10 @@ Route::middleware(['auth', 'verified'])->name('admin.')->prefix('admin')
    ->group(function () {
          Route::get('/', [DashboardController::class, 'index'])
          ->name('dashboard');
+        Route::resource('posts', PostController::class)->parameters(['posts' => 'post:slug']);
    });
+
+
 // Route::middleware('auth')->group(function () {
 //     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
 //     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
