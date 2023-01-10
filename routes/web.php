6b14d0c257eec2cefd18f2496494bc93ab85aa77
GioGreco <grecogiorgio.97@gmail.com>
2023-01-10 12:56:04 +0100

    use postcontroller in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\PostController;
+
 use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
