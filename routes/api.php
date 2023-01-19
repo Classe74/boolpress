2aca31bf774a250d8a091541afbb667b909844a4
klelia <clelia.fradella@gmail.com>
2023-01-19 11:50:05 +0100

    Lead entity e mail

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\LeadController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -21,3 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('posts', [PostController::class,'index']);
 Route::get('posts/{slug}', [PostController::class,'show']);
+
+Route::post('/contacts', [LeadController::class, 'store']);
