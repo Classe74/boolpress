1dae1c7cd20ddd39ca611db221e44fcc93203801
klelia <clelia.fradella@gmail.com>
2023-01-26 09:19:55 +0100

    pp

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\LeadController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,9 +13,14 @@ use App\Http\Controllers\Api\PostController;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
+
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('posts', [PostController::class,'index']);
+Route::get('posts/{slug}', [PostController::class,'show']);
+
+Route::post('/contacts', [LeadController::class, 'store']);
