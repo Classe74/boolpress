363c8ccfe57b0a2a3919fe7a4acc42450298707b
klelia <clelia.fradella@gmail.com>
2023-01-26 10:44:00 +0100

    aggiunta riga vuota ad api.php

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+
 Route::get('posts', [PostController::class, 'index']);
 Route::get('posts/{slug}', [PostController::class, 'show']);
 
