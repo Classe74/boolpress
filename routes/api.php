f02fa8e0b82d7e16f13b793acd4a6e9bd3353060
klelia <clelia.fradella@gmail.com>
2024-09-25 17:54:25 +0200

    ora

--- routes/api.php
+++ routes/api.php
@@ -24,4 +24,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('posts', [PostController::class, 'index']);
 Route::get('posts/{slug}', [PostController::class, 'show']);
 
+
 Route::post('/contacts', [LeadController::class, 'store']);
